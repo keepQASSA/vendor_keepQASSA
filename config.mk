3d9127820f2779d1ce7f1a87ff1956ee315fd842
Mocarafee <harif.rp05@gmail.com>
2022-08-15 23:56:22 +0000

    keepQASSA: Only build Shady Launcher on vanilla variant [1/2]
    keepQASSA: Prepare for overlay separation
    
    Revert "keepQASSA: cleanup some Google packages [1/2]"
    
    This reverts commit 76785aabdfe25913882a5d8459a6ddddd8aa45d0.
    
    keepQASSA: Rename overlay directory to avoid confusion

--- config.mk
+++ config.mk
@@ -79,9 +79,24 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.error.receiver.system.apps=com.google.android.gms \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent
 
-# Include product overlays
+# GApps
+ifeq ($(WITH_GAPPS),true)
+# Prebuilt Packages
+PRODUCT_PACKAGES += \
+    MatchmakerPrebuiltPixel4 \
+    NexusLauncherRelease \
+    NexusWallpapersStubPrebuilt2019Static \
+    PixelThemesStub2019 \
+    SettingsIntelligenceGooglePrebuilt
+
+# Include product overlays for Pixel.
 PRODUCT_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay-pixel
+else
+# Include product overlays for Shady Launcher.
+PRODUCT_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-launcher3
+endif
 
 # Inherit from audio config
 $(call inherit-product, vendor/keepQASSA/config/audio.mk)
