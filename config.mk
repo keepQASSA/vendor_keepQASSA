ef99b6d871173e34b1d511e0a8e9dbd5e29bba38
ghou8s <ghou8s@gmail.com>
2022-09-17 11:45:25 +0530

    Revert "pixelstyle: Use Pixel charger animation"
    This reverts commit e13bf8afb8cdc46cb002dc7d2961054f29f8082e.

--- config.mk
+++ config.mk
@@ -14,11 +14,6 @@
 
 LOCAL_PATH := vendor/keepQASSA
 
-# Offline charger
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    product_charger_res_images
-
 # Bootanimation
 # ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
 #     PRODUCT_COPY_FILES += $(LOCAL_PATH)/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
