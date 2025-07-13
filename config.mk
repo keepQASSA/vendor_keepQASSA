9ca15784765a6362c88692480833c442d22cc086
ghou8s <ghou8s@gmail.com>
2025-07-13 02:34:44 +0000

    keepQASSA: overlay: set Rounded corner to large by default

--- config.mk
+++ config.mk
@@ -30,9 +30,9 @@ else
     PRODUCT_COPY_FILES += $(LOCAL_PATH)/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 endif
 
-# Gestures
+# Overlays
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.gnonymous.gvisualmod.urm_l
 
 # Gboard configuration
 PRODUCT_PRODUCT_PROPERTIES += \
